969873e2938fdb2a623f4731a00d66a88635c1b3
Divyajyoti <50514405+divyajyoti09@users.noreply.github.com>
2024-05-18 01:24:41 +0530

    Inserting missing bracket

--- ESIGMA.py
+++ ESIGMA.py
@@ -3,24 +3,24 @@ from pycbc.types.timeseries import TimeSeries
 
 def IMRESIGMAHM_td(**input_params):
     hp, hc = enigma_utils.get_imr_esigma_waveform(**input_params)
-    hp_ts = TimeSeries(hp, input_params['delta_t')
+    hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
     return(hp_ts, hc_ts)
 
 def IMRESIGMA_td(**input_params):
     hp, hc = enigma_utils.get_imr_esigma_waveform(**input_params, modes_to_use=[(2, 2)])
-    hp_ts = TimeSeries(hp, input_params['delta_t')
+    hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
     return(hp_ts, hc_ts)
 
 def InspiralESIGMAHM_td(**input_params):
     _, hp, hc = enigma_utils.get_inspiral_esigma_waveform(**input_params)
-    hp_ts = TimeSeries(hp, input_params['delta_t')
+    hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
     return(hp_ts, hc_ts)
 
 def InspiralESIGMA_td(**input_params):
     hp, hc = enigma_utils.get_inspiral_esigma_waveform(**input_params, modes_to_use=[(2, 2)])
-    hp_ts = TimeSeries(hp, input_params['delta_t')
+    hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
     return(hp_ts, hc_ts)
