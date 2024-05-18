d62b5f4fd10b4a15a9f02a685fb23315f4e86ef2
Divyajyoti <50514405+divyajyoti09@users.noreply.github.com>
2024-05-19 01:22:53 +0530

    Setting path of gwnr using sys.modules

--- post_install.py
+++ post_install.py
@@ -10,8 +10,8 @@ def main():
         try:
             import gwnr
             
-            # Path to esigma_utils.py
-            esigma_utils_path = os.path.join(os.path.dirname(gwnr.__file__), 'waveform', 'esigma_utils.py')
+            # Path to esigma_utils.py using sys.modules
+            esigma_utils_path = os.path.join(os.path.dirname(sys.modules['gwnr'].__file__), 'waveform', 'esigma_utils.py')
             
             # Read the content of esigma_utils.py
             with open(esigma_utils_path, 'r') as file:
@@ -22,12 +22,15 @@ def main():
             lal_data_path_line = f'os.environ["LAL_DATA_PATH"] = "{lal_data_path}"\n'
             
             # Check if the lines already exist
-            if import_os_line in lines:
+            import_os_exists = any(line.strip() == import_os_line.strip() for line in lines)
+            lal_data_path_exists = any(line.strip() == lal_data_path_line.strip() for line in lines)
+            
+            if import_os_exists:
                 print(f'The line "{import_os_line.strip()}" already exists in esigma_utils.py')
-            if lal_data_path_line in lines:
+            if lal_data_path_exists:
                 print(f'The line "{lal_data_path_line.strip()}" already exists in esigma_utils.py')
             
-            if import_os_line not in lines or lal_data_path_line not in lines:
+            if not import_os_exists or not lal_data_path_exists:
                 # Find the index where to insert the LAL_DATA_PATH line
                 insert_index = None
                 for index, line in enumerate(lines):
@@ -36,9 +39,9 @@ def main():
                 
                 if insert_index is not None:
                     # Insert the lines if they don't already exist
-                    if import_os_line not in lines:
+                    if not import_os_exists:
                         lines.insert(insert_index, import_os_line)
-                    if lal_data_path_line not in lines:
+                    if not lal_data_path_exists:
                         lines.insert(insert_index + 1, lal_data_path_line)
                     
                     # Write the updated content back to esigma_utils.py
