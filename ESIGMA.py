e48f2747e07df32140207a4dc08c0f98f90ac912
Divyajyoti <50514405+divyajyoti09@users.noreply.github.com>
2024-05-18 12:17:42 +0530

    Taper hp and hc by default when calling the waveforms

--- ESIGMA.py
+++ ESIGMA.py
@@ -1,6 +1,31 @@
 from gwnr.waveform import esigma_utils
 from pycbc.types.timeseries import TimeSeries
 
+try:
+    # some versions of pycbc include td_taper in pycbc.waveform
+    from pycbc.waveform import td_taper
+except:
+    # some other versions of pycbc include td_taper in pycbc.waveform.utils
+    from pycbc.waveform.utils import td_taper
+
+def taper_signal(signal, beta=5):
+    """
+    Returns tapered signal between start and start+0.4s.
+
+    Parameters
+    ----------
+
+    signal : PyCBC TimeSeries
+    beta : The beta parameter to use for the Kaiser window. See scipy.signal.kaiser for details. Default is 5.
+
+    Returns
+    -------
+
+    signal_tapered : PyCBC TimeSeries
+    """
+    signal_tapered = td_taper(signal, signal.sample_times[0], signal.sample_times[0]+0.4, beta=beta)
+    return(signal_tapered)
+
 def IMRESIGMAHM_td(**input_params):
     """
     Returns tapered time domain gravitational polarizations for IMRESIGMAHM waveform containing all (l,|m|) modes available.
@@ -11,18 +36,21 @@ def IMRESIGMAHM_td(**input_params):
     Takes the same parameters as pycbc.waveform.get_td_waveform().
     
     Returns
-        -------
+    -------
 
-        hplus: PyCBC TimeSeries
-            The plus-polarization of the waveform in time domain tapered from start to 0.4s.
-        hcross: PyCBC TimeSeries
-            The cross-polarization of the waveform in time domain tapered from start to 0.4s.
+    hplus : PyCBC TimeSeries
+        The plus-polarization of the waveform in time domain tapered from start to 0.4s.
+    hcross : PyCBC TimeSeries
+        The cross-polarization of the waveform in time domain tapered from start to 0.4s.
     """
     
     hp, hc = esigma_utils.get_imr_esigma_waveform(**input_params)
     hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
-    return(hp_ts, hc_ts)
+    
+    hp_tapered = taper_signal(hp_ts)
+    hc_tapered = taper_signal(hc_ts)
+    return(hp_tapered, hc_tapered)
 
 def IMRESIGMA_td(**input_params):
     """
@@ -34,17 +62,20 @@ def IMRESIGMA_td(**input_params):
     Takes the same parameters as pycbc.waveform.get_td_waveform().
     
     Returns
-        -------
+    -------
 
-        hplus: PyCBC TimeSeries
-            The plus-polarization of the waveform in time domain tapered from start to 0.4s.
-        hcross: PyCBC TimeSeries
-            The cross-polarization of the waveform in time domain tapered from start to 0.4s.
+    hplus : PyCBC TimeSeries
+        The plus-polarization of the waveform in time domain tapered from start to 0.4s.
+    hcross : PyCBC TimeSeries
+        The cross-polarization of the waveform in time domain tapered from start to 0.4s.
     """
     hp, hc = esigma_utils.get_imr_esigma_waveform(**input_params, modes_to_use=[(2, 2)])
     hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
-    return(hp_ts, hc_ts)
+    
+    hp_tapered = taper_signal(hp_ts)
+    hc_tapered = taper_signal(hc_ts)
+    return(hp_tapered, hc_tapered)
 
 def InspiralESIGMAHM_td(**input_params):
     """
@@ -56,17 +87,20 @@ def InspiralESIGMAHM_td(**input_params):
     Takes the same parameters as pycbc.waveform.get_td_waveform().
     
     Returns
-        -------
+    -------
 
-        hplus: PyCBC TimeSeries
-            The plus-polarization of the waveform in time domain tapered from start to 0.4s.
-        hcross: PyCBC TimeSeries
-            The cross-polarization of the waveform in time domain tapered from start to 0.4s.
+    hplus : PyCBC TimeSeries
+        The plus-polarization of the waveform in time domain tapered from start to 0.4s.
+    hcross : PyCBC TimeSeries
+        The cross-polarization of the waveform in time domain tapered from start to 0.4s.
     """
     _, hp, hc = esigma_utils.get_inspiral_esigma_waveform(**input_params)
     hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
-    return(hp_ts, hc_ts)
+    
+    hp_tapered = taper_signal(hp_ts)
+    hc_tapered = taper_signal(hc_ts)
+    return(hp_tapered, hc_tapered)
 
 def InspiralESIGMA_td(**input_params):
     """
@@ -78,14 +112,17 @@ def InspiralESIGMA_td(**input_params):
     Takes the same parameters as pycbc.waveform.get_td_waveform().
     
     Returns
-        -------
+    -------
 
-        hplus: PyCBC TimeSeries
-            The plus-polarization of the waveform in time domain tapered from start to 0.4s.
-        hcross: PyCBC TimeSeries
-            The cross-polarization of the waveform in time domain tapered from start to 0.4s.
+    hplus : PyCBC TimeSeries
+        The plus-polarization of the waveform in time domain tapered from start to 0.4s.
+    hcross : PyCBC TimeSeries
+        The cross-polarization of the waveform in time domain tapered from start to 0.4s.
     """
     hp, hc = esigma_utils.get_inspiral_esigma_waveform(**input_params, modes_to_use=[(2, 2)])
     hp_ts = TimeSeries(hp, input_params['delta_t'])
     hc_ts = TimeSeries(hc, input_params['delta_t'])
-    return(hp_ts, hc_ts)
+    
+    hp_tapered = taper_signal(hp_ts)
+    hc_tapered = taper_signal(hc_ts)
+    return(hp_tapered, hc_tapered)
